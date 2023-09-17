e6493d93f57fd81e0f32bc04432b12e4016e3503
Reiyn <98770205+iamreiyn@users.noreply.github.com>
2023-09-17 17:12:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./css/index.css"
+import Terminal from './js/Terminal';
+
+export default function App() {
+    return (
+        <div>
+            
+            <Terminal />
+        </div>
+    );
+}
